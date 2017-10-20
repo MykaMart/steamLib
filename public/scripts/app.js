3db423a9f14b1fa4fd6f5c55fd5b242d58dff1ac
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-10-20 11:33:40 -0500

    added html to run app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,4 @@
-// for (let i = 0; i < moviesCSV.length; i++) {
-// 		let query = `?t=${moviesCSV[i].Title.replace(/ /g, "+")}&y=${moviesCSV[i].Year}&apikey=d2f48936`;
-
+cosnole.log("Javascipt connected")
 		$.ajax({
 			      url: " http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=F45B8E320A2BED0BF9730245EA191005&steamid=76561197960434622&format=json"
 			      dataType: "JSON",
