a5b0aaa11c3bdb205777203110ec97618d72e273
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-10-23 08:55:54 -0500

    fixed typo

--- models/playerModel.js
+++ models/playerModel.js
@@ -6,7 +6,7 @@ const playerSchema = new mongoose.Schema({
 steamID: String, //we will be using the id numbers to concatenate urls
 name: String,
 avatar: String,
-library: [Library.schema]
+library: [Library.schema],
 friends: [Friend.schema]
 });
 
